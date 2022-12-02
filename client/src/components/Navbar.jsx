589e113a7efb044f0299a7549bc3e72f4ab249f2
Sebastian Letelier <sebalete10@gmail.com>
2022-12-02 16:08:53 -0300

    arreglo de errores y crud front finalizado

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
-export const Navbar = () => {
+
+function Navbar() {
   return (
-    <div>
-        <h1>MERN ToDo Tasks App</h1>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/new">Create Task</Link>
-            </li>
-        </ul>
+    <div className="bg-neutral-800 flex justify-between px-20 py-4">
+      <Link to="/" className="text-white font-bold">
+        <h1>React MySQL</h1>
+      </Link>
+
+      <ul className="flex gap-x-1">
+        <li>
+          <Link to="/" className="bg-slate-200 px-2 py-1">Home</Link>
+        </li>
+        <li>
+          <Link to="/new" className="bg-teal-200 px-2 py-1">Create task</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
+
+export default Navbar;
--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-//archivo barril para los imports.
-export * from './TaskForm';
-export * from './TasksPage';
-export * from './NotFound';
-
-
