0e1a13dad0e3bcbf18ff26b5c41e1c0a1de35081
Sebastian Letelier <sebalete10@gmail.com>
2022-12-02 12:16:41 -0300

    backend crud tasks finished

---
+++ db/db.sql
@@ -0,0 +1,9 @@
+
+--Cración de la tabla tareas en mysql
+-- CREATE TABLE tasks (
+--     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(200) NOT NULL,
+--     description VARCHAR(200),
+--     done BOOLEAN NOT NULL DEFAULT 0,
+--     createAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- );
